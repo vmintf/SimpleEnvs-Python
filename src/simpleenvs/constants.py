406db19cdf5c6e48cf982030f8661c44cf5ce202
vmintf <vmintf@gmail.com>
2025-06-06 18:15:38 +0900

    chore: bump version to 1.1.3 in constants and project metadata

--- src/simpleenvs/constants.py
+++ src/simpleenvs/constants.py
@@ -341,7 +341,7 @@ FEATURES = {
 # =============================================================================
 
 # Version information
-VERSION = "1.1.2"
+VERSION = "1.1.3"
 API_VERSION = "1.0"
 MIN_PYTHON_VERSION = (3, 7)
 
